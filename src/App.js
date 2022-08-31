945a676aeed99e5472fb002c782e5cde4592447e
Frostbite22 <landoulsiferes@outlook.com>
2022-08-31 21:31:59 +0100

    generic json response

--- src/App.js
+++ src/App.js
@@ -1,57 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {useEffect, useState} from 'react';
 
-
-function handleClick(title)
-{
-    axios({
-      method: "GET",
-      url : `/${title}`
-    })
-    .then(res => {
-      const model = res.data[`${title}`]
-      model.map((elm) => 
-      {
-        console.log(elm)
-      })
-    }
-    )
-    
-}
 
 function NavBar(props) 
 {
+  function handleClick(title,setData)
+  {
+      axios({
+        method: "GET",
+        url : `/${title}`
+      })
+      .then(res => {
+        const model = res.data[`${title}`]
+        setData(JSON.parse(JSON.stringify(model)));
+      }) 
+  }
+
   const titles = props.titles ;
   return(
     <div className="topnav">
       {
         titles.map((title) => {
-        return <a href={title} key={title} onClick={() => handleClick(title)}>{title}</a>
+        return <a key={title} onClick={() => handleClick(title,props.setData)}>{title}</a>
         })
       }
     </div>
+    
   );
 }
 
+
 function App() {
+  
+  const [data,setData] = useState([]);
+
   return (
     <div className="App">
-      <NavBar titles={["artists","venues","shows","genres"]}/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar titles={["artists","venues","shows","genres"]} setData={setData} />
+      <div>
+      {
+        data.map((element)=> {
+          return (
+            <>
+            <div key={element.id}>
+              {
+                Object.entries(element).map( ([key,value]) => 
+                { 
+                  return (
+                    
+                    <div>
+                      {key} : {typeof value !== 'object'? value : null}
+                    </div>
+                  )
+                }
+                ) 
+              }
+            </div>
+            <br></br>
+           </>
+          )
+        })
+      }
+    </div>
+
     </div>
   );
 }
