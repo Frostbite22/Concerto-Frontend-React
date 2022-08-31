d62c4d7f18e71fba5ca3e485544d048cd98a8cd7
Frostbite22 <landoulsiferes@outlook.com>
2022-08-31 13:23:09 +0100

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+function NavBar(props) 
+{
+  const titles = props.titles ;
+
+  return(
+    <div class="topnav">
+      {
+        titles.map((title) => {
+        return <a href={title}>{title}</a>
+        })
+      }
+    </div>
+
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <NavBar titles={["artists","venues","show","genres"]}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
